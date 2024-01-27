5de2c5949428f4346b1135db52101122f7794015
combstyler <tylerinnesscombs@gmail.com>
2024-01-26 22:56:47 -0800

    Match config files to  fresh Astro install. Remove unused configs. Install postcss-preset-env for oklch colors.

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,4 +1,13 @@
 // postcss.config.cjs
+const postcssPresetEnv = require('postcss-preset-env');
+
 module.exports = {
-  plugins: [require('autoprefixer')],
+  plugins: [
+    /* other plugins */
+    /* remove autoprefixer if you had it here, it's part of postcss-preset-env */
+    postcssPresetEnv({
+      /* pluginOptions */
+      stage: 2,
+    }),
+  ],
 };
