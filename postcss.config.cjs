2f2fb59b10bdd1eedff42b2ccbd5cb5d6e0d8b09
combstyler <tylerinnesscombs@gmail.com>
2024-02-18 09:09:41 -0800

    Cleanup repo. Update packages and configs.

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,12 +1,8 @@
-// postcss.config.cjs
 const postcssPresetEnv = require('postcss-preset-env');
 
 module.exports = {
   plugins: [
-    /* other plugins */
-    /* remove autoprefixer if you had it here, it's part of postcss-preset-env */
     postcssPresetEnv({
-      /* pluginOptions */
       stage: 2,
     }),
   ],
